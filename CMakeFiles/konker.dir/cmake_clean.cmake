16ceb06448bb121da94c859ba193695bff00da7a
Rogier <rogierderuijter@hotmail.com>
2016-10-19 21:47:29 +0200

    lekker

---
+++ CMakeFiles/konker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/konker.dir/src/main.cc.o"
+  "CMakeFiles/konker.dir/src/person.cc.o"
+  "CMakeFiles/konker.dir/src/inlezen.cc.o"
+  "build/konker.pdb"
+  "build/konker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/konker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
